584dbeecc7e8fc0af42ac823d032d53cc4497808
diomeiwandany <diomeiwandany@gmail.com>
2024-07-15 20:56:20 +0700

    fix: openai class

--- server/openai.js
+++ server/openai.js
@@ -3,29 +3,51 @@ const { OpenAI } = require("openai");
 require("dotenv").config();
 
 // export default async function openAI(
-async function openAI(category, totalQuestion) {
-    const openai = new OpenAI({
-        apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-    });
+class OpenAIClass {
+    static openai() {
+        try {
+            const openai = new OpenAI({
+                apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+            });
+            return openai;
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-    const completion = await openai.chat.completions.create({
-        messages: [
-            {
-                role: "system",
-                content: `
-        Buat ${JSON.stringify(
-            totalQuestion
-        )} pertanyaan seperti family 100 dengan kategori ${JSON.stringify(
-                    category
-                )}, dan buat menjadi format sebagai berikut:
-            [{id: serial,question: "",answers: [{answer: "",score: point,revealed: false}}]}]
-        `,
-            },
-        ],
-        model: "gpt-3.5-turbo",
-    });
+    static async createQuestion(category, totalQuestion) {
+        try {
+            const completion = await this.openai().chat.completions.create({
+                messages: [
+                    {
+                        role: "system",
+                        content: `
+                            Buat ${JSON.stringify(
+                                totalQuestion
+                            )} pertanyaan seperti family 100 dengan kategori ${JSON.stringify(
+                            category
+                        )}, dan buat menjadi format sebagai berikut:
+                                [{id: serial,question: "",answers: [{answer: "",score: point,revealed: false}}]}]
+                            `,
+                    },
+                ],
+                model: "gpt-3.5-turbo",
+            });
 
-    return completion.choices[0].message.content ?? "";
+            return completion.choices[0].message.content ?? "";
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    static async compareAnswer(answerByUser, realAnswer) {
+        try {
+        } catch (error) {
+            console.error(error);
+        }
+    }
 }
 
-module.exports = openAI;
+OpenAIClass.createQuestion("film horor", 5).then(console.log);
+
+module.exports = OpenAIClass;
