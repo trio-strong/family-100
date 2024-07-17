ae231180ecbb6568d29417e43431b93236ac629a
MUHAMMAD RAIHAN <muhammadraihan@mail.ugm.ac.id>
2024-07-18 02:56:42 +0700

    feat: fix promt

--- server/openai.js
+++ server/openai.js
@@ -22,8 +22,15 @@ class OpenAIClass {
           {
             role: "system",
             content: `
-            Buat ${totalQuestion} pertanyaan seperti family 100 dengan kategori ${category}, dan buat menjadi format sebagai berikut:
-                [{id: serial,question: "",answers: [{answer: "",score: point,revealed: false}}]}]
+            Saya memiliki kata atau frasa: ${answerByUser}. Di bawah ini adalah kumpulan data yang terdiri dari list kata atau frase:
+            ${stringAnswers}
+
+            Tolong evaluasi kata atau frase mana dari kumpulan list tersebut yang paling mirip dengan jawaban tersebut berdasarkan maknanya. Keluarkan hasilnya dalam format JSON sebagai berikut:
+            {
+            "status": boolean, # Apakah ada kemiripan atau tidak
+            "matched": "<kata atau frase terdekat>", # Kata atau frase yang paling sesuai
+            "percentage": <nilai kemiripan dalam persentase> # Persentase kemiripan
+            }
             `,
           },
         ],
